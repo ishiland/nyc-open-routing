d04a054bb13b702e1ef13c28567bf454d5dfe478
Ian Shiland <ian.shiland@madisonspringfield.com>
2020-08-05 19:15:06 -0400

    updatte geosupport version

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,26 +1,22 @@
 #!/bin/sh
 set -e
 
-GEOSUPPORT_DIRECTORY=version-19b_19.2
+# TODO: write a function to retrieve the downloaded geosupport sub-directory name.
+# The $LION env var can then be used to get the same version of Geosupport.
 
+GEOSUPPORT_DIRECTORY=version-20c_20.3
+# https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/linux_geo20c_20_3.zip
 if [ -d /api/geosupport ]; then
     echo "Geosupport directory detected, not downloading.";
 else
     echo "Downloading Geosupport"
     mkdir geosupport
-    curl -O https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/linux_geo19b_19_2.zip \
+    curl -O https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/linux_geo20c_20_3.zip \
     && unzip *.zip -d /api/geosupport \
     && rm *.zip;
 fi
 
-
 export GEOFILES=/api/geosupport/${GEOSUPPORT_DIRECTORY}/fls/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/api/geosupport/${GEOSUPPORT_DIRECTORY}/lib/
 
-if [ "$FLASK_ENV" -eq "production" ]; then
-    echo "running production api server";
-    gunicorn app --bind=5000 --workers=5 --threads=3;
-else
-    echo "starting development api server";
-    python3 app.py;
-fi
+python3 app.py
